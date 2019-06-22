cec830865da3f2500cdc691f9efc61c31ffe1df4
Calli Crase <callilogancrase@gmail.com>
2019-06-22 12:00:50 -0600

    working

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,5 +1,4 @@
-$(document).ready(function(){
-console.log("working");
+//$(document).ready(function(){
 
 //var youtubeAPIKey = AIzaSyC6sAc1Rkq7nUSRyGN3XmPC4UZjiLITs64;
 var alphabet = [a,b,c,d,e,f,g];
@@ -10,35 +9,31 @@ var d =["deer", "dog", "donkey", "dolphin"];
 var e =["elephant", "eagle", "emu"];
 var f =["fox", "flamingo", "ferret"];
 var g =["gecko", "gerbil", "giraffe"];
-var h =[];
-var i =[];
-var j =[];
-var k =[];
-var l =[];
-var m =[];
-var n =[];
-var o =[];
-var p =[];
-var q =[];
-var r =[];
-var s =[];
-var t =[];
-var u =[];
-var v =[];
-var w =[];
-var x =[];
-var y =[];
-var z =[];
+// var h =[];
+// var i =[];
+// var j =[];
+// var k =[];
+// var l =[];
+// var m =[];
+// var n =[];
+// var o =[];
+// var p =[];
+// var q =[];
+// var r =[];
+// var s =[];
+// var t =[];
+// var u =[];
+// var v =[];
+// var w =[];
+// var x =[];
+// var y =[];
+// var z =[];
 
-$(".btn").on("click", function(){
+$("body").on("click", ".btn", function(event){
+    event.preventDefault();
     var userClick = $(this).attr("id");
-    for(var i =0; i< alphabet.length; i++){
-        if(userClick === alphabet[i]){
-            
-        }
-    }
-
-})
-
-
 });
+
+console.log(alphabet);
+
+// });
